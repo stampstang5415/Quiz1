193ad4cccb8c5628f323a538a59f68b676825116
stampstang5415 <stampstang5415@gmail.com>
2018-08-30 14:24:13 +0700

    s++

--- ss.js
+++ ss.js
@@ -2,3 +2,18 @@ document.addEventListener('prechange', function(event) {
     document.querySelector('ons-toolbar .center')
       .innerHTML = event.tabItem.getAttribute('label');
   });
+  var prev = function() {
+    var carousel = document.getElementById('carousel');
+    carousel.prev();
+  };
+  
+  var next = function() {
+    var carousel = document.getElementById('carousel');
+    carousel.next();
+  };
+  
+  ons.ready(function() {
+    var carousel = document.addEventListener('postchange', function(event) {
+      console.log('Changed to ' + event.activeIndex)
+    });
+  });
